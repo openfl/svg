571976e9ac3b68102be77738d9a1f23bbc71b9e4
Joshua Granick <joshua@joshuagranick.com>
2014-04-30 10:38:22 -0700

    Compile fix

--- format/gfx/Gradient.hx
+++ format/gfx/Gradient.hx
@@ -23,11 +23,7 @@ class Gradient
    }
 
    public var type:GradientType;
-   #if flash
-      public var colors:Array<UInt>;
-   #else
-      public var colors:Array<Int>;
-   #end
+   public var colors:Array<UInt>;
    public var alphas:Array<Float>;
    public var ratios:Array<Int>;
    public var matrix: Matrix;
